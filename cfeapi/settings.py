dffd9c3c43030412706e966de1ed4605fe5e0f8e
litsynp <nocte55is@gmail.com>
2021-01-07 01:54:05 +0900

    feat: implement a simple image upload but in the hard way

--- cfeapi/settings.py
+++ cfeapi/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,3 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# '...../dev/restapi/
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static-server', 'media-root')
+MEDIA_URL = '/media/'
--- scripts/cfe_rest_framework_api.py
+++ scripts/cfe_rest_framework_api.py
@@ -1,7 +1,9 @@
 import json
+import os
 import requests
 
 ENDPOINT = 'http://127.0.0.1:8000/api/status/'
+IMAGE_PATH = os.path.join(os.getcwd(), 'logo.png')
 
 
 def do(method='get', data={}, is_json=True):
@@ -14,7 +16,32 @@ def do(method='get', data={}, is_json=True):
     return r
 
 
+def do_img(method='post', data={}, is_json=True, img_path=None):
+    headers = {}
+    if is_json:
+        headers['content-type'] = 'application/json'
+        data = json.dumps(data)
+
+    if img_path is not None:
+        with open(img_path, 'rb') as image:
+            file_data = {
+                'image': image
+            }
+            r = requests.request(method, ENDPOINT, data=data,
+                                 headers=headers, files=file_data)
+    else:
+        r = requests.request(method, ENDPOINT, data=data, headers=headers)
+    print(r.text)
+    return r
+
+
+do_img(method='post', data={'user': 1, 'content': ''},
+       is_json=False, img_path=IMAGE_PATH)
+
+do_img(method='put', data={'id': 12, 'user': 1, 'content': 'some new great content'},
+       is_json=False, img_path=IMAGE_PATH)
+
 # do(data={'id': 9})
-do(method='delete', data={'id': 500})
+# do(method='delete', data={'id': 500})
 # do(method='put', data={'id': 9, 'content': 'some cool new content', 'user': 1})
 # do(method='post', data={'content': 'some cool new content', 'user': 1})

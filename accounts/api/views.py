30f82467843a1dec77ef4250c9e521fded59629b
litsynp <nocte55is@gmail.com>
2021-01-09 20:08:20 +0900

    feat: implement generics to simplify register API

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -4,6 +4,9 @@ from rest_framework import permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework_jwt.settings import api_settings
+from rest_framework import generics
+
+from .serializers import UserRegisterSerializer
 
 jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
 jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
@@ -38,27 +41,7 @@ class AuthAPIView(APIView):
         return Response({'detail': 'Invalid credentials'}, status=401)
 
 
-class RegisterAPIView(APIView):
+class RegisterAPIView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegisterSerializer
     permission_classes = [permissions.AllowAny]
-
-    def post(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            return Response({'detail': 'You are already registered and are authenticated'}, status=400)
-
-        data = request.data
-        username = data.get('username')
-        email = data.get('email')
-        password = data.get('password')
-        password2 = data.get('password2')
-
-        qs = User.objects.filter(
-            Q(username__iexact=username) | Q(email__iexact=username))
-        if qs.exists():
-            return Response({'detail': 'This user already exists'}, status=401)
-        elif password != password2:
-            return Response({'detail': 'Password must match'}, status=401)
-        else:
-            user = User.objects.create(username=username, email=email)
-            user.set_password(password)
-            user.save()
-            return Response({'detail': 'Thank you for registering. Please verify your email.'}, status=201)
